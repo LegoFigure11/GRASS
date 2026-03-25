2f6a713cc3b5c507384a54f70d8e59d575efb461
LegoFigure11 <24732684+LegoFigure11@users.noreply.github.com>
2026-03-25 23:34:17 +1100

    Add Keyboard handling for all languages

---
+++ GRASS.Core/KeyboardEntry/KeyboardTools.cs
@@ -0,0 +1,166 @@
+﻿using System.Text;
+using PKHeX.Core;
+using SysBot.Base;
+using static GRASS.Core.KeyboardEntry.Keyboards;
+
+namespace GRASS.Core.KeyboardEntry;
+
+public static class KeyboardTools
+{
+    private static LanguageID _lang = LanguageID.English;
+    private static string[][] _keyboard = GetKeyboard(_lang);
+    public static void SetKeyboard(LanguageID lang)
+    {
+        _keyboard = GetKeyboard(lang);
+        _lang = lang;
+    }
+
+    private static bool CheckKeyExists(char k) => _keyboard.Any(kbd => kbd.Any(m => m.Contains(k)));
+    private static bool CheckKeyExistsOnKeyboard(char k, int mode)
+    {
+        return _keyboard[mode].Any(row => row.Contains(k));
+    }
+
+    private static int GetKeyRow(char c, int kbd, int first = 0)
+    {
+        var k = _keyboard[kbd];
+
+        if (k[first].Contains(c)) return first;
+
+        var i = 0;
+        for (; i < k.Length; i++)
+        {
+            if (k[i].Contains(c)) break;
+        }
+
+        return i;
+    }
+
+    private static (int col, int row) GetKeyColRow(char c, int kbd, int row, int first = 0)
+    {
+        var k = _keyboard[kbd];
+
+        var check = row;
+        var forward = 0;
+        var backward = 0;
+        var found = false;
+        for (var i = 0; i < k.Length; i++)
+        {
+            if (k[check][first] == c) {
+                forward = check;
+                found = true;
+                break;
+            }
+            check = (check + 1) % k.Length;
+        }
+
+        check = row;
+        for (var i = k.Length - 1; i > 0; i--)
+        {
+            if (k[check][first] == c)
+            {
+                backward = check;
+                found = true;
+                break;
+            }
+            check = ((check - 1 + k.Length) % k.Length);
+        }
+
+        if (found) return (Math.Abs(row - forward) < Math.Abs(row - backward)) ? (first, forward) : (first, backward); 
+
+        return (k[row].IndexOf(c), row);
+
+    }
+
+    private static bool CheckLength(string text, LanguageID lang) => lang switch
+    {
+        LanguageID.Japanese => text.Length <= 5,
+        _                   => text.Length <= 7,
+    };
+
+    public static List<SwitchButton> GetInputsForName(string text)
+    {
+        if (!CheckLength(text, _lang)) throw new ArgumentException($"Name {text} is too long for the connected game language!");
+
+        if (string.IsNullOrEmpty(text)) throw new ArgumentException($"No OT Name provided!");
+
+        // Normalize out Dakuten/Handakuten if applicable
+        if (_lang == LanguageID.Japanese) text = text.Normalize(NormalizationForm.FormD);
+
+        // start at 0, 0 on Upper
+        var kbdindex = 0;
+        var row = 0;
+        var col = 0;
+
+        List<SwitchButton> inputs = [];
+
+        foreach (var c in text)
+        {
+            if (!CheckKeyExists(c)) throw new ArgumentException($"Character: {c} does not exist in the specified keyboard set!");
+
+            var i = 0;
+            while (!CheckKeyExistsOnKeyboard(c, kbdindex))
+            {
+                kbdindex = (kbdindex + 1) % _keyboard.Length;
+                i++;
+
+                // If switching through a keyboard with less cols, set to the max col
+                col = Math.Min(col, _keyboard[kbdindex][row].Length - 1);
+            }
+
+            for (var j = 0; j < i; j++)
+            {
+                inputs.Add(SwitchButton.MINUS);
+            }
+
+            // Now on the correct keyboard
+            var tmpRow = GetKeyRow(c, kbdindex, row);
+            var (keyCol, keyRow) = GetKeyColRow(c, kbdindex, tmpRow, col);
+
+            var rowDir = keyRow >= row ? SwitchButton.DDOWN : SwitchButton.DUP;
+            var colDir = keyCol >= col ? SwitchButton.DRIGHT : SwitchButton.DLEFT;
+
+            var rowDiff = Math.Abs(keyRow - row);
+            var colDiff = Math.Abs(keyCol - col);
+
+            if (rowDiff > _keyboard[kbdindex].Length / 2)
+            {
+                rowDiff = _keyboard[kbdindex].Length - rowDiff;
+                rowDir = rowDir == SwitchButton.DDOWN ? SwitchButton.DUP : SwitchButton.DDOWN;
+            }
+
+            var extra = false;
+            var extraDir = SwitchButton.DRIGHT;
+            if (colDiff > (_keyboard[kbdindex][row].Length / 2) + 1)
+            {
+                colDiff = _keyboard[kbdindex][row].Length - colDiff;
+                colDir = colDir == SwitchButton.DRIGHT ? SwitchButton.DLEFT : SwitchButton.DRIGHT;
+                extra = true;
+                extraDir = colDir;
+            }
+
+
+            for (var x = 0; x < rowDiff; x++)
+            {
+                inputs.Add(rowDir);
+            }
+
+            for (var y = 0; y < colDiff; y++)
+            {
+                inputs.Add(colDir);
+            }
+
+            if (extra) inputs.Add(extraDir);
+
+            row = keyRow;
+            col = keyCol;
+
+            inputs.Add(SwitchButton.A);
+        }
+
+        inputs.Add(SwitchButton.PLUS);
+        inputs.Add(SwitchButton.A);
+
+        return inputs;
+    }
+}
---
+++ GRASS.Core/KeyboardEntry/Keyboards.cs
@@ -0,0 +1,109 @@
+﻿using PKHeX.Core;
+
+namespace GRASS.Core.KeyboardEntry;
+
+internal static class Keyboards
+{
+    private static readonly string[] EN_Upper = [
+        "ABCDEF .",
+        "GHIJKL ,",
+        "MNOPQRS ",
+        "TUVWXYZ ",
+    ];
+    private static readonly string[] EN_Lower = [
+        "abcdef .",
+        "ghijkl ,",
+        "mnopqrs ",
+        "tuvwxyz ",
+    ];
+    private static readonly string[] EN_Other = [
+        "01234 ",
+        "56789 ",
+        "!?♂♀/-",
+        "…“”‘’ ",
+    ];
+
+    private static readonly string[] FR_Upper = [
+        "ABCDEFGH.",
+        "IJKLMNOP,",
+        "QRSTUVWX ",
+        "YZ  -    "
+    ];
+
+    private static readonly string[] FR_Lower = [
+        "abcdefgh.",
+        "ijklmnop,",
+        "qrstuvwx ",
+        "yz  -    ",
+    ];
+
+    private static readonly string[] FR_Other = [
+        "01234 ",
+        "56789 ",
+        "!?♂♀/ ",
+        "…«»‘’ ",
+    ];
+
+    private static readonly string[] DE_Upper = [
+        "ABCDEFGH.",
+        "IJKLMNOP,",
+        "QRSTUVWX ",
+        "YZ  ÄÖÜ  "
+    ];
+
+    private static readonly string[] DE_Lower = [
+        "abcdefgh.",
+        "ijklmnop,",
+        "qrstuvwx ",
+        "yz  äöü  ",
+    ];
+
+    private static readonly string[] DE_Other = [
+        "01234 ",
+        "56789 ",
+        "!?♂♀/-",
+        "…“”‘’ ",
+    ];
+
+    private static readonly string[] IT_Upper = EN_Upper;
+    private static readonly string[] IT_Lower = EN_Lower;
+    private static readonly string[] IT_Other = EN_Other;
+
+    private static readonly string[] ES_Upper = EN_Upper;
+    private static readonly string[] ES_Lower = EN_Lower;
+    private static readonly string[] ES_Other = EN_Other;
+
+    private static readonly string[] JP_Hiragana = [
+        "あいうえお なにぬねの やゆよ！？  ",
+        "かきくけこ はひふへほ わをん \u3099\u309A ",
+        "さしすせそ まみむめも ゃゅょっー  ",
+        "たちつてと らりるれろ ぁぃぅぇぉ  ",
+    ];
+
+    private static readonly string[] JP_Katakana = [
+        "アイウエオ ナニヌネノ ヤユヨ！？  ",
+        "カキクケコ ハヒフヘホ ワヲン \u3099\u309A ",
+        "サシスセソ マミムメモ ャュョッー  ",
+        "タチツテト ラリルレロ ァィゥェォ  "
+    ];
+
+    private static readonly string[] JP_Latin = [
+        "ＡＢＣＤＥＦＧＨＩＪＫＬＭＮＯＰＱＲＳ",
+        "ＴＵＶＷＸＹＺ ０１２３４５６７８９ ",
+        "ａｂｃｄｅｆｇｈｉｊｋｌｍｎｏｐｑｒｓ",
+        "ｔｕｖｗｘｙｚ 。・⋯『』「」／♂♀ "
+    ];
+
+
+
+    public static string[][] GetKeyboard(LanguageID lang = LanguageID.English) => lang switch
+    {
+        LanguageID.Japanese => [JP_Hiragana, JP_Katakana, JP_Latin],
+        LanguageID.English => [EN_Upper, EN_Lower, EN_Other],
+        LanguageID.French => [FR_Upper, FR_Lower, FR_Other],
+        LanguageID.Italian => [IT_Upper, IT_Lower, IT_Other],
+        LanguageID.German => [DE_Upper, DE_Lower, DE_Other],
+        LanguageID.Spanish => [ES_Upper, ES_Lower, ES_Other],
+        _ => throw new NotImplementedException(),
+    };
+}
